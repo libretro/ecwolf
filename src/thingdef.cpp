cb6f14d4380f59a0841d3ae364012fdc7b933a34
Braden Obrzut <admin@maniacsvault.net>
2010-01-18 08:24:21 +0000

    - Doing more work on the DECORATE system.  In particular I started on doing native actors and inheritance.  I believe things are vary slowly falling into place now.

--- src/thingdef.cpp
+++ src/thingdef.cpp
@@ -10,15 +10,48 @@ using namespace std;
 #define DEFINE_FLAG(prefix, flag, type, variable) { prefix##_##flag, #flag, (int)(size_t)&((type*)1)->variable - 1 }
 const FlagDef flags[] =
 {
-	DEFINE_FLAG(FL, SHOOTABLE, Actor, flags),
-	DEFINE_FLAG(FL, BONUS, Actor, flags),
-	DEFINE_FLAG(FL, NEVERMARK, Actor, flags),
-	DEFINE_FLAG(FL, VISABLE, Actor, flags)
+	DEFINE_FLAG(FL, SHOOTABLE, AActor, flags),
+	DEFINE_FLAG(FL, BONUS, AActor, flags),
+	DEFINE_FLAG(FL, NEVERMARK, AActor, flags),
+	DEFINE_FLAG(FL, VISABLE, AActor, flags)
 };
+
+map<string, ClassDef *> ClassDef::classTable;
+
+ClassDef::ClassDef()
+{
+	defaultInstance = new AActor(this);
+}
+
+ClassDef::~ClassDef()
+{
+	delete defaultInstance;
+}
+
+const ClassDef *ClassDef::FindClass(const std::string &className)
+{
+	map<string, ClassDef *>::const_iterator iter = classTable.find(className);
+	if(iter == classTable.end())
+		return NULL;
+	return iter->second;
+}
+
+bool ClassDef::IsDecendantOf(const ClassDef *parent) const
+{
+	const ClassDef *currentParent = parent;
+	while(currentParent != NULL)
+	{
+		if(currentParent == parent)
+			return true;
+		currentParent = currentParent->parent;
+	}
+	return false;
+}
 
 void ClassDef::LoadActors()
 {
 	printf("ClassDef: Loading actor definitions.\n");
+	atexit(&ClassDef::UnloadActors);
 
 	int lastLump = 0;
 	int lump = 0;
@@ -26,14 +59,41 @@ void ClassDef::LoadActors()
 	{
 		ParseDecorateLump(lump);
 	}
+	DumpClasses();
 }
 
 void ClassDef::ParseActor(Scanner &sc)
 {
-	ClassDef *newClass = new ClassDef();
-
+	// Read the header
 	sc.MustGetToken(TK_Identifier);
+	ClassDef *newClass = classTable[sc.str];
+	bool previouslyDefined = newClass != NULL;
+	if(!previouslyDefined)
+	{
+		newClass = new ClassDef();
+		classTable[sc.str] = newClass;
+	}
+	bool native = false;
 	newClass->name = sc.str;
+	if(sc.CheckToken(':'))
+	{
+		sc.MustGetToken(TK_Identifier);
+		newClass->parent = FindClass(sc.str);
+		if(newClass->parent == NULL)
+			sc.ScriptError("Could not find parent actor '%s'\n", sc.str.c_str());
+	}
+	else if(!previouslyDefined) // If no class was specified to inherit from, inherit from AActor, but not for AActor.
+		newClass->parent = NATIVE_CLASS(Actor)
+	if(sc.CheckToken(TK_Identifier))
+	{
+		if(stricmp(sc.str.c_str(), "native") == 0)
+			native = true;
+		else
+			sc.ScriptError("Unknown keyword '%s'.\n", sc.str.c_str());
+	}
+	if(previouslyDefined && !native)
+		sc.ScriptError("Actor '%s' already defined.\n", newClass->name.c_str());
+
 	sc.MustGetToken('{');
 	while(!sc.CheckToken('}'))
 	{
@@ -154,4 +214,90 @@ void ClassDef::ParseDecorateLump(int lumpNum)
 		else
 			sc.ScriptError("Unknown thing section '%s'.", sc.str.c_str());
 	}
+}
+
+void ClassDef::UnloadActors()
+{
+	for(map<string, ClassDef *>::iterator iter = classTable.begin();iter != classTable.end();iter++)
+	{
+		delete iter->second;
+	}
+}
+
+template<class T>
+const ClassDef *ClassDef::DeclareNativeClass(const char* className, const ClassDef *parent)
+{
+	ClassDef *definition = classTable[className];
+	if(definition == NULL)
+	{
+		definition = new ClassDef();
+		classTable[className] = definition;
+	}
+	definition->name = className;
+	definition->parent = parent;
+	delete definition->defaultInstance;
+	definition->defaultInstance = new T(definition);
+	return definition;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+const ClassDef *AActor::__StaticClass = ClassDef::DeclareNativeClass<AActor>("Actor", NULL);
+
+AActor::AActor(const ClassDef *type) : classType(type)
+{
+}
+
+
+class AWeapon : public AActor
+{
+	DECLARE_NATIVE_CLASS(Weapon, Actor)
+};
+IMPLEMENT_CLASS(Weapon, Actor)
+
+////////////////////////////////////////////////////////////////////////////////
+
+void ClassDef::DumpClasses()
+{
+	struct ClassTree
+	{
+		public:
+			ClassTree(const ClassDef *classType) : child(NULL), next(NULL), thisClass(classType)
+			{
+				ClassTree **nextChild = &child;
+				for(map<string, ClassDef *>::iterator iter = classTable.begin();iter != classTable.end();iter++)
+				{
+					if(iter->second->parent == classType)
+					{
+						*nextChild = new ClassTree(iter->second);
+						nextChild = &(*nextChild)->next;
+					}
+				}
+			}
+
+			~ClassTree()
+			{
+				if(child != NULL)
+					delete child;
+				if(next != NULL)
+					delete next;
+			}
+
+			void Dump(int spacing)
+			{
+				string myName = string(spacing*2, ' ') + thisClass->name;
+				printf("%s\n", myName.c_str());
+				if(child != NULL)
+					child->Dump(spacing+1);
+				if(next != NULL)
+					next->Dump(spacing);
+			}
+
+			ClassTree		*child;
+			ClassTree		*next;
+			const ClassDef	*thisClass;
+	};
+
+	ClassTree root(FindClass("Actor"));
+	root.Dump(0);
 }
--- src/thingdef.h
+++ src/thingdef.h
@@ -2,15 +2,43 @@
 #define __THINGDEF_H__
 
 #include <string>
+#include <map>
 
 #include "scanner.hpp"
 
 class ClassDef;
 
-class Actor
+#define DECLARE_NATIVE_CLASS(name, parent) \
+	friend class ClassDef; \
+	protected: \
+		A##name(const ClassDef *classType) : A##parent(classType) {} \
+	public: \
+		static const ClassDef *__StaticClass;
+#define IMPLEMENT_CLASS(name, parent) \
+	const ClassDef *A##name::__StaticClass = ClassDef::DeclareNativeClass<A##name>(#name, A##parent::__StaticClass);
+#define NATIVE_CLASS(name) A##name::__StaticClass;
+
+class AActor
 {
+	friend class ClassDef;
+
 	public:
+		struct Frame
+		{
+			public:
+				char	frame[4];
+				int		duration;
+				void	(*action);
+				void	(*thinker);
+		};
+
 		unsigned int flags;
+
+		static const ClassDef *__StaticClass;
+	protected:
+		AActor(const ClassDef *type);
+
+		const ClassDef	*classType;
 };
 
 struct FlagDef
@@ -24,21 +52,37 @@ struct FlagDef
 class ClassDef
 {
 	public:
-		struct Frame
-		{
-			public:
-				char	frame[4];
-				int		duration;
-				void	(*action);
-				void	(*thinker);
-		};
+		ClassDef();
+		~ClassDef();
 
-		static void	LoadActors();
+		bool					IsDecendantOf(const ClassDef *parent) const;
+
+		/**
+		 * Use with IMPLEMENT_CLASS to add a natively defined class.
+		 */
+		template<class T>
+		static const ClassDef	*DeclareNativeClass(const char* className, const ClassDef *parent);
+
+		/**
+		 * Prints the implemented classes in a tree.  This is not designed to 
+		 * be fast since it's debug information more than anything.
+		 */
+		static void				DumpClasses();
+
+		static const ClassDef	*FindClass(const std::string &className);
+		static void				LoadActors();
+		static void				UnloadActors();
+
 	protected:
-		static void ParseActor(Scanner &sc);
-		static void ParseDecorateLump(int lumpNum);
+		static void	ParseActor(Scanner &sc);
+		static void	ParseDecorateLump(int lumpNum);
 
-		std::string	name;
+		static std::map<std::string, ClassDef *>	classTable;
+
+		std::string		name;
+		const ClassDef	*parent;
+
+		AActor			*defaultInstance;
 };
 
 #endif /* __THINGDEF_H__ */
