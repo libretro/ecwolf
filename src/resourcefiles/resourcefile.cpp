bedc70f78a1870731aa7ef5c99a4b348eb6b24f1
Braden Obrzut <admin@maniacsvault.net>
2013-01-08 14:03:50 -0500

    - Start of Mac Wolf support: Read MacBin.

--- src/resourcefiles/resourcefile.cpp
+++ src/resourcefiles/resourcefile.cpp
@@ -259,9 +259,10 @@ FResourceFile *CheckVGAGraph(const char *filename, FileReader *file, bool quiet)
 FResourceFile *CheckVSwap(const char *filename, FileReader *file, bool quiet);
 FResourceFile *CheckAudiot(const char *filename, FileReader *file, bool quiet);
 FResourceFile *CheckGamemaps(const char *filename, FileReader *file, bool quiet);
+FResourceFile *CheckMacBin(const char *filename, FileReader *file, bool quiet);
 
-#define COUNTOF_FUNCS 11
-static CheckFunc funcs[COUNTOF_FUNCS] = { CheckWad, CheckZip, Check7Z, CheckPak, CheckGRP, CheckRFF, CheckAudiot, CheckVGAGraph, CheckVSwap, CheckGamemaps, CheckLump };
+#define COUNTOF_FUNCS 12
+static CheckFunc funcs[COUNTOF_FUNCS] = { CheckWad, CheckZip, Check7Z, CheckPak, CheckGRP, CheckRFF, CheckMacBin, CheckAudiot, CheckVGAGraph, CheckVSwap, CheckGamemaps, CheckLump };
 
 FResourceFile *FResourceFile::OpenResourceFile(const char *filename, FileReader *file, bool quiet)
 {
