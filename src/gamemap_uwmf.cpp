63247b598e016690b5d0654219b513585ec46f73
Braden Obrzut <admin@maniacsvault.net>
2016-05-09 21:31:59 -0400

    - Added ability to reference action specials by name in xlat/UWMF.

--- src/gamemap_uwmf.cpp
+++ src/gamemap_uwmf.cpp
@@ -184,8 +184,31 @@ void TextMapParser::ParseTrigger(Scanner &sc, MapTrigger &trigger)
 	}
 	else CheckKey("action")
 	{
-		sc.MustGetToken(TK_IntConst);
-		trigger.action = sc->number;
+		if(sc.CheckToken(TK_IntConst))
+		{
+			trigger.action = sc->number;
+
+			// Warn on first use of deprecated special number.
+			static bool deprSpecial = false;
+			if(!deprSpecial)
+			{
+				deprSpecial = true;
+				sc.ScriptMessage(Scanner::WARNING, "Use of action special number is deprecated. Use names instead.");
+			}
+		}
+		else
+		{
+			sc.MustGetToken(TK_StringConst);
+			Specials::LineSpecials num = Specials::LookupFunctionNum(sc->str);
+			if(num != Specials::NUM_POSSIBLE_SPECIALS)
+				trigger.action = num;
+			else
+			{
+				if(!sc->str.IsEmpty())
+					sc.ScriptMessage(Scanner::WARNING, "Could not resolve action special '%s'.", sc->str.GetChars());
+				trigger.action = 0;
+			}
+		}
 	}
 	else CheckKey("arg0")
 	{
@@ -490,6 +513,14 @@ class UWMFParser : public TextMapParser
 			{
 				if(sc.CheckToken(TK_IntConst))
 				{
+					// Deprecated use of Doom Editor Number
+					static bool deprEdNum = false;
+					if(!deprEdNum)
+					{
+						deprEdNum = true;
+						sc.ScriptMessage(Scanner::WARNING, "Deprecated use of editor number. Use class name instead.");
+					}
+
 					if(const ClassDef *cls = ClassDef::FindClass(sc->number))
 						thing.type = cls->GetName();
 					else if(sc->number >= 1 && sc->number <= SMT_NumThings)
