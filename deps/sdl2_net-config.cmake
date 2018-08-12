f86917e7d5540ce59481ccbfb2b00781e86b0d52
Braden Obrzut <admin@maniacsvault.net>
2018-08-12 03:43:37 -0400

    - Move SDL vendor detection to LocateSDL2.cmake since Mercurial subrepositories don't work the way I thought they did, so we'll want to make the user explicitly opt-in to using them. Unless they don't have SDL installed of course.

--- deps/sdl2_net-config.cmake
+++
@@ -1,3 +0,0 @@
-# This file provides a config for find_package so that we can use our vendored
-# libraries or the system libraries with minimal boilerplate code.
-add_library(SDL2::SDL2_net ALIAS SDL2_net)
